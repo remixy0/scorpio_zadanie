0ef593056371c133f6787bfb9021383a5301520c
Stanislaw Sikora <remixy0@onet.pl>
2025-10-06 23:49:05 +0200

    poczatek

--- src/solution/solution.cpp
+++ src/solution/solution.cpp
@@ -11,9 +11,17 @@ int solver(std::shared_ptr<backend_interface::Tester> tester, bool preempt) {
 
   // Short example you can remove it
   std::cout << (preempt ? "Preempt" : "Queue") << '\n';
+
   auto motor1 = tester->get_motor_1();
   auto motor2 = tester->get_motor_2();
   auto commands = tester->get_commands();
+
+  commands->add_data_callback([](const Point& target) {
+    std::cout << "Cel: x=" << target.x
+              << " y=" << target.y
+              << " z=" << target.z << std::endl;
+  });
+
   motor1->add_data_callback([](const uint16_t& data) {
     std::cout << "Motor 1 data: " << static_cast<int>(data) << "\n";
   });
@@ -23,8 +31,11 @@ int solver(std::shared_ptr<backend_interface::Tester> tester, bool preempt) {
   commands->add_data_callback([](const Point& point) {
     std::cout << "Command point: (" << point.x << ", " << point.y << ", " << point.z << ")\n";
   });
+
+
   motor1->send_data(100);
   motor2->send_data(-50);
+
   std::this_thread::sleep_for(std::chrono::milliseconds(250));
   //
   return 0;
