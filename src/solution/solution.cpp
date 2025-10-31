15cef1c19ba0f36ea31b936abb07ee18f6712354
Stanislaw Sikora <remixy0@onet.pl>
2025-10-31 21:09:41 +0100

    kosmetyka

--- src/solution/solution.cpp
+++ src/solution/solution.cpp
@@ -9,7 +9,7 @@ using namespace std;
 constexpr long double PI = 3.141592653589793238462643L;
 constexpr long double E = 2.718281828459045235360287L;
 
-double procent_bledu = 0.2;
+double errorPercent = 0.2;
 
 
 class Points {
@@ -23,17 +23,17 @@ public:
         this->z = z;
     }
 
-    float katX() const {
+    float angleX() const {
         return atan2(y,x)/ (2*PI) * 4095;
 
     }
 
-    float katY() const {
-        float promien = sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
-        return acos(z/promien)/ (2*PI) * 4095;
+    float angleY() const {
+        float radius = sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
+        return acos(z/radius)/ (2*PI) * 4095;
     }
 
-    void isDone(){
+    void markDone(){
         done = true;
     }
 
@@ -41,32 +41,32 @@ public:
 
 
 
-float wzor(float enkoder,float kat) {
-    int mnoznik;
+float speedFormula(float encoder,float angle) {
+    int multiplier;
 
-    float roznica = abs(enkoder - kat);
-    float speed = 127 * (1 - pow(E,-roznica/20));
+    float difference = abs(encoder - angle);
+    float speed = 127 * (1 - pow(E,-difference/20));
 
-    if (enkoder < kat) {
-        mnoznik = 1;
+    if (encoder < angle) {
+        multiplier = 1;
     }else {
-        mnoznik =  -1;
+        multiplier =  -1;
     }
-    return speed * mnoznik;
+    return speed * multiplier;
 }
 
-void ustawienieSilnikow(Points& p, uint16_t& encoder_value_X, uint16_t& encoder_value_Y,std::shared_ptr<backend_interface::Tester> tester) {
+void setMotors(Points& p, uint16_t& encoder_value_X, uint16_t& encoder_value_Y,std::shared_ptr<backend_interface::Tester> tester) {
     auto motor1 = tester->get_motor_1();
     auto motor2 = tester->get_motor_2();
     this_thread::sleep_for(chrono::milliseconds(10));
-    cout << "Cel: " << p.x << " " << p.y << " " << p.z << endl;
+    cout << "Target: " << p.x << " " << p.y << " " << p.z << endl;
 
-    while (abs(p.katX() - encoder_value_X) > (4095 * procent_bledu / 100) || abs(p.katY() - encoder_value_Y) > (4095 * procent_bledu / 100)) {
-        motor1->send_data(wzor(encoder_value_X, p.katX()));
-        motor2->send_data(wzor(encoder_value_Y, p.katY()));
+    while (abs(p.angleX() - encoder_value_X) > (4095 * errorPercent/ 100) || abs(p.angleY() - encoder_value_Y) > (4095 * errorPercent / 100)) {
+        motor1->send_data(speedFormula(encoder_value_X, p.angleX()));
+        motor2->send_data(speedFormula(encoder_value_Y, p.angleY()));
     }
-    p.isDone();
-    cout << "Osiagnieto: " << p.x << " " << p.y << " " << p.z << endl;
+    p.markDone();
+    cout << "Reached: " << p.x << " " << p.y << " " << p.z << endl;
 }
 
 
@@ -92,44 +92,43 @@ int solver(std::shared_ptr<backend_interface::Tester> tester, bool preempt) {
       current_point = Points(target.x,target.y,target.z);
       didPrint = false;
       if (preempt) {
-          cout << "zmiana celu na: x=" << target.x
+           cout << "changing target to: x=" << target.x
                 << " y=" << target.y
                 << " z=" << target.z << endl;
-          }else {
-              cout << "dodano cel do kolejki: x=" << target.x
-                              << " y=" << target.y
-                              << " z=" << target.z << endl;
-
-          }
+       } else {
+           cout << "added target to queue: x=" << target.x
+                << " y=" << target.y
+                << " z=" << target.z << endl;
+       }
   });
 
     motor1->add_data_callback([&encoder_value_X](const uint16_t& value){
       encoder_value_X = value;
-      // cout << "Enkoder X: " << encoder_value_X << endl;
+      // cout << "Encoder X: " << encoder_value_X << endl;
   });
 
   motor2->add_data_callback([&encoder_value_Y](const uint16_t& value){
      encoder_value_Y = value;
-     // cout << "Enkoder Y: " << encoder_value_Y << endl;
+     // cout << "Encoder Y: " << encoder_value_Y << endl;
  });
 
 
 
     if (preempt) {
         while (true){
-            if ((abs(current_point.katX() - encoder_value_X) < (4095 * procent_bledu / 100) && abs(current_point.katY() - encoder_value_Y) < (4095 * procent_bledu / 100)) && !didPrint) {
-                cout << "Osiagnieto: " << current_point.x << " " << current_point.y << " " << current_point.z << endl;
+            if ((abs(current_point.angleX() - encoder_value_X) < (4095 * errorPercent / 100) && abs(current_point.angleY() - encoder_value_Y) < (4095 * errorPercent / 100)) && !didPrint) {
+                cout << "Reached: " << current_point.x << " " << current_point.y << " " << current_point.z << endl;
                 didPrint = true;
             }
-            motor1->send_data(wzor(encoder_value_X, current_point.katX()));
-            motor2->send_data(wzor(encoder_value_Y, current_point.katY()));
+            motor1->send_data(speedFormula(encoder_value_X, current_point.angleX()));
+            motor2->send_data(speedFormula(encoder_value_Y, current_point.angleY()));
         }
     }
 
     if (!preempt) {
         while (true){
             for (auto& p : points) {
-                if (!p.done) ustawienieSilnikow(p,encoder_value_X,encoder_value_Y,tester);
+                if (!p.done) setMotors(p,encoder_value_X,encoder_value_Y,tester);
             }
         }
     }
