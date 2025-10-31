663a160e73ff2b169c8b582685a268c178f390e9
Stanislaw Sikora <remixy0@onet.pl>
2025-10-31 20:15:49 +0100

    dodanie funkcji do obslugi silnikow

--- src/solution/solution.cpp
+++ src/solution/solution.cpp
@@ -9,6 +9,37 @@ using namespace std;
 constexpr long double PI = 3.141592653589793238462643L;
 constexpr long double E = 2.718281828459045235360287L;
 
+double procent_bledu = 1;
+
+
+class Points {
+public:
+    int x,y,z;
+    bool done = false;
+
+    Points(int x, int y, int z) {
+        this->x = x;
+        this->y = y;
+        this->z = z;
+    }
+
+    float katX() const {
+        return atan2(y,x)/ (2*PI) * 4095;
+
+    }
+
+    float katY() const {
+        float promien = sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
+        return acos(z/promien)/ (2*PI) * 4095;
+    }
+
+    void isDone(){
+        done = true;
+    }
+
+};
+
+
 
 float wzor(float enkoder,float kat) {
     int mnoznik;
@@ -24,28 +55,21 @@ float wzor(float enkoder,float kat) {
     return speed * mnoznik;
 }
 
-class Points {
-private:
-    float promien;
+void ustawienieSilnikow(Points& p, uint16_t& encoder_value_X, uint16_t& encoder_value_Y,std::shared_ptr<backend_interface::Tester> tester) {
+    auto motor1 = tester->get_motor_1();
+    auto motor2 = tester->get_motor_2();
 
-public:
-    int x,y,z;
+    cout << "Cel: " << p.x << " " << p.y << " " << p.z << endl;
 
-    Points(int x, int y, int z) {
-        this->x = x;
-        this->y = y;
-        this->z = z;
+    while (abs(p.katX() - encoder_value_X) > (4095 * procent_bledu / 100) || abs(p.katY() - encoder_value_Y) > (4095 * procent_bledu / 100)) {
+        motor1->send_data(wzor(encoder_value_X, p.katX()));
+        motor2->send_data(wzor(encoder_value_Y, p.katY()));
     }
+    p.isDone();
+    cout << "Osiagnieto: " << p.x << " " << p.y << " " << p.z << endl;
+}
 
-    float katX(){
-        return atan((y/x))/ (2*PI) * 4095;
-    }
 
-    float katY(){
-        promien = sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
-        return acos(z/promien) / (2*PI) * 4095;
-    }
-};
 
 
 
@@ -56,43 +80,54 @@ int solver(std::shared_ptr<backend_interface::Tester> tester, bool preempt) {
     uint16_t encoder_value_X = 0;
     uint16_t encoder_value_Y = 0;
     std::list<Points> points;
+    Points current_point = Points(0,0,0);
 
   auto motor1 = tester->get_motor_1();
   auto motor2 = tester->get_motor_2();
   auto commands = tester->get_commands();
 
-  commands->add_data_callback([&points](const Point& target) {
+  commands->add_data_callback([&points, &current_point, &preempt](const Point& target) {
       points.emplace_back(target.x,target.y,target.z);
-      cout << "Cel: x=" << target.x
-              << " y=" << target.y
-              << " z=" << target.z << endl;
-
-    });
+      current_point = Points(target.x,target.y,target.z);
+      if (preempt) {
+          cout << "zmiana celu na: x=" << target.x
+                << " y=" << target.y
+                << " z=" << target.z << endl;
+          }
+  });
 
     motor1->add_data_callback([&encoder_value_X](const uint16_t& value){
       encoder_value_X = value;
-      cout << "Enkoder 1: " << encoder_value_X << endl;
+      // cout << "Enkoder 1: " << encoder_value_X << endl;
   });
 
   motor2->add_data_callback([&encoder_value_Y](const uint16_t& value){
      encoder_value_Y = value;
-     cout << "Enkoder 2: " << encoder_value_Y << endl;
+     // cout << "Enkoder 2: " << encoder_value_Y << endl;
  });
 
 
 
-    for (const auto& p : points)
-        p.print();
+    if (preempt) {
+        while (true){
+            if (abs(current_point.katX() - encoder_value_X) < (4095 * procent_bledu / 100) || abs(current_point.katY() - encoder_value_Y) < (4095 * procent_bledu / 100)) {
+                cout << "Osiagnieto: " << current_point.x << " " << current_point.y << " " << current_point.z << endl;
+            }
+            motor1->send_data(wzor(encoder_value_X, current_point.katX()));
+            motor2->send_data(wzor(encoder_value_Y, current_point.katY()));
+        }
+    }
 
-    while (true) {
-    motor1->send_data(wzor(encoder_value_X,));
-    motor2->send_data(wzor(encoder_value_Y,));
-    this_thread::sleep_for(chrono::milliseconds(100));
-}
+    if (!preempt) {
+        while (true){
+            for (auto& p : points) {
+                if (!p.done) ustawienieSilnikow(p,encoder_value_X,encoder_value_Y,tester);
+            }
+        }
+    }
 
 
-
-  return 0;
+    return 0;
 
 
 
